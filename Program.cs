af9b53a36cc957c2d4037a5a238df3e7f4f646d4
Galin Kutsarov <galinkutsarov@abv.bg>
2024-09-17 18:04:17 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,31 +2,51 @@
 {
     static void Main()
     {
-        int num = int.Parse(Console.ReadLine());
-        int lastnumber = (num) % 10;
-
-        switch (lastnumber)
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int num3 = int.Parse(Console.ReadLine());
+        
+        if (num1 >= num2 && num1 >= num3)
         {
-            case 0:
-                Console.WriteLine("zero"); break;
-            case 1:
-                Console.WriteLine("one"); break;
-            case 2:
-                Console.WriteLine("two"); break;
-            case 3:
-                Console.WriteLine("three"); break;
-            case 4:
-                Console.WriteLine("four"); break;
-            case 5:
-                Console.WriteLine("five"); break;
-            case 6:
-                Console.WriteLine("six"); break;
-            case 7:
-                Console.WriteLine("seven"); break;
-            case 8:
-                Console.WriteLine("eight"); break;
-            case 9:
-                Console.WriteLine("nine"); break;
+            Console.WriteLine(num1);
+            if (num2 >= num3)
+            {
+                Console.WriteLine(num2);
+                Console.WriteLine(num3);
+            }
+            else
+            {
+                Console.WriteLine(num3);
+                Console.WriteLine(num2);
+            }
+        }
+        else if (num2 >= num3 && num2 >= num1)
+        {
+            Console.WriteLine(num2);
+            if (num3 >= num1)
+            {
+                Console.WriteLine(num3);
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                Console.WriteLine(num1);
+                Console.WriteLine(num3);
+            }
+        }
+        else if (num3 >= num1 && num3 >= num2)
+        {
+            Console.WriteLine(num3);
+            if (num1 >= num2)
+            {
+                Console.WriteLine(num1);
+                Console.WriteLine(num2);
+            }
+            else
+            {
+                Console.WriteLine(num2);
+                Console.WriteLine(num1);
+            }
         }
     }
 }
